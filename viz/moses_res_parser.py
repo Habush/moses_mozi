fd7835d9c55f9ebf7e788e10bfad13aa7c3f6930
Abdulrahman Semrie <hsamireh@gmail.com>
2018-02-13 18:02:08 +0300

    Added function to parse model files

--- viz/moses_res_parser.py
+++ viz/moses_res_parser.py
@@ -2,12 +2,13 @@ __author_ = 'Xabush Semrie'
 
 from anytree.exporter import JsonExporter
 from lark import Lark
+from tree_transform import MosesTree
 
 grammer = r"""
     
         model: "(EquivalenceLink (stv 1.0 1.0)" "(" "PredicateNode" mname ")" func ")" 
         
-        func: lpar func_name param+ rpar
+        func: lpar func_name param+ rpar | lpar param_name rpar
         
         param: "(" param_name ")" | func
         
@@ -29,36 +30,44 @@ grammer = r"""
         %ignore WS
 """
 
-moses_parser = Lark(grammer, start='model', parser="lalr")
+
+def read_scm_file(path):
+    output = []
+    in_model = False
+    current = ""
+    with open(path, 'r') as fp:
+        for line in fp:
+            if line.rstrip() == ";;end_model":
+                in_model =  False
+                output.append(current)
+                current = ""
+            if in_model:
+                current += line
+            if line.rstrip() == ";;begin_model":
+                in_model = True
 
 
 
+    return output
 
+def parse_moses_result(path):
+    moses_parser = Lark(grammer, start='model', parser="lalr")
 
-
-
-text = """(EquivalenceLink (stv 1.0 1.0)
-    (PredicateNode "temp_file_5a757b012ae73d5ddf306289:moses_model_10")
-    (OrLink (AndLink (OrLink (NotLink (PredicateNode "PRR14L")) (PredicateNode "RUNX2")) (NotLink (PredicateNode "SCFD2")) (NotLink (PredicateNode "TRIO.1"))) (AndLink (PredicateNode "TRIO.1") (NotLink (PredicateNode "PRR14L")))))"""
-
-text2 = """(EquivalenceLink (stv 1.0 1.0)
-    (PredicateNode "moses_model_00")
-    (AndLink (NotLink (PredicateNode "SCFD2")) (PredicateNode "TRIO.1") (NotLink (PredicateNode "PRR14L"))))"""
-
-text3 = """(EquivalenceLink (stv 1.0 1.0)
-    (PredicateNode "temp_file_5a757b012ae73d5ddf306289:moses_model_02")
-    (AndLink (OrLink (NotLink (PredicateNode "SCFD21")) (PredicateNode "TRIO.12")) (NotLink (PredicateNode "PRR14L2"))))"""
-
-text4 = """(EquivalenceLink (stv 1.0 1.0)
-    (PredicateNode "moses_model_10")
-    (OrLink (AndLink (OrLink (NotLink (PredicateNode "PRR14L")) (PredicateNode "RUNX2")) (NotLink (PredicateNode "SCFD2")) (NotLink (PredicateNode "TRIO.1"))) (AndLink (PredicateNode "TRIO.1") (NotLink (PredicateNode "PRR14L")))))"""
-
-if __name__ == "__main__":
-    tree = moses_parser.parse(text4)
-
-    moses = MosesTree()
-    tree = moses.transform(tree)
-
+    models = read_scm_file(path)
     exporter = JsonExporter(indent=0)
 
-    print(exporter.export(moses.root))
+    output = []
+
+    for model in models:
+        tree = moses_parser.parse(model.rstrip())
+        tree_transformer = MosesTree()
+        tree_transformer.transform(tree)
+        result = exporter.export(tree_transformer.root)
+        output.append(result.strip())
+
+    return output
+
+if __name__ == "__main__":
+    test_path = "/home/xabush/mozi_datasets/result/test.scm"
+
+    print(parse_moses_result(test_path)[17])
